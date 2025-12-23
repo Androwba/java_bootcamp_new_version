5da9e14827380465f3013ea17bba461c403186ba
Androwba <145433993+Androwba@users.noreply.github.com>
2025-12-24 00:05:00 +0300

    Add files via upload

---
+++ AP1_Jv_T01-1/src/exercise0/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+    id("application")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(18))
+}
+
+application {
+    mainClass.set("org.example.Main")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
